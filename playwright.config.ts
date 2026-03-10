91f1327fde146333766f88b24a11e12632ee8465
JustWallage <just@wallage.nl>
2026-03-10 09:43:43 +0100

    fix: playwright url in config instead of extract from command

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    baseURL: process.env.BASE_URL || "http://localhost:5173",
+    baseURL: process.env.CI ? 'https://staging.jaw-finance.pages.dev' : 'http://localhost:5173',
     trace: "on-first-retry",
   },
 });
